2201588cd3378c3a21a961640914fff4a655f041
yosuke.funado <music_beats_spirit@icloud.com>
2021-10-03 09:35:01 +0900

    フラッシュメッセージを追加

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -36,11 +36,18 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        
+
         em.close();
-        
+
         request.setAttribute("tasks", tasks);
-        
+
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する(セッションスコープからは削除)
+        if (request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
     }
